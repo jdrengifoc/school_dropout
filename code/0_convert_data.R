09b7ea481e85aafbb540455c8a50fb6bdc5509f2
Maria Jose Guerrero <mariajoseguerrero@MacBook-Pro-de-Maria.local>
2024-12-03 07:19:48 -0500

    Creation dictionary ministery

--- code/0_convert_data.R
+++ code/0_convert_data.R
@@ -9,7 +9,7 @@ create_folder <- function(folders, recursive = T) {
   }
 }
 
-# SIMAT -------------------------------------------------------------------
+# SIMAT 2004 - 2022 -------------------------------------------------------------------
 
 files <- list.files('Deserción Escolar/data/SIMAT/Matriculas validadas por año',
                     full.names = T) %>% str_remove("\\~\\$")
@@ -33,9 +33,36 @@ for (file in files) {
   }
 }
 
+files <- list.files(new_folder)
+
 raw_dict_path <- file.path(DICTS_FOLDER, 'raw_SIMAT.xlsx')
 create_partial_dictionary(folder = new_folder, files = files, 
                           dict_path = raw_dict_path, verbose = T, overwrite = F)
 sort_partial_dictionary(raw_dict_path, overwrite = T)
+
+# SIMAT 2017-2023------------------
+
+parquet_folder_2017_2023 <- "/Users/mariajoseguerrero/Documents/Secretaria de Educación/Deserción Escolar/data/SIMAT/SIMAT 2017-2023"
+output_path <- file.path(DICTS_FOLDER, "Raw_SIMAT_2017_2023.xlsx")
+
+files <- list.files(parquet_folder_2017_2023)
+
+raw_dict_path <- file.path(DICTS_FOLDER, 'raw_SIMAT_2017_2023.xlsx')
+
+
+create_partial_dictionary(folder = parquet_folder_2017_2023, files = files, 
+                          dict_path = raw_dict_path, verbose = T, overwrite = T)
+sort_partial_dictionary(raw_dict_path, overwrite = T)
+
+
+
+
+
+
+
+
+
+
+
 
 
